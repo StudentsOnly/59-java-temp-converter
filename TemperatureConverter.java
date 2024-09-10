0f658cb7c181bcc3b784f8bed161ed523b59902e
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-09-10 11:47:47 +0200

    throw IllegalArgumenException in convertTemperature

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,6 +1,9 @@
 public class TemperatureConverter {
 
     public double convertTemperature(double temperature, String unit) {
+        if (!"CF".contains(unit.toUpperCase())) {
+            throw new IllegalArgumentException("Invalid scale " + unit);
+        }
         return unit.equalsIgnoreCase("C") ? celsiusToFahrenheit(temperature) :
                 unit.equalsIgnoreCase("F") ? fahrenheitToCelsius(temperature) : -1;
     }
