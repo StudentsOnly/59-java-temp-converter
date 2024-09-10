5385523cabbbed4ce66d2096a9e7161f807f7a75
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-09-10 12:05:41 +0200

    add user interface to main

--- Main.java
+++ Main.java
@@ -1,3 +1,32 @@
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Main{
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter the temperature you want to convert: ");
+            double temperature = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.println("Enter the temperature unit: ");
+            String unit = scanner.nextLine().toUpperCase().trim();
+            double convertedTemp = TemperatureConverter.convertTemperature(temperature, unit);
+            System.out.println("Converted temperature: " +
+                    "%.2f".formatted(convertedTemp) +
+                    (unit.equals("C") ? " F" : " C"));
+        } catch (IllegalArgumentException | InputMismatchException e) {
+            System.out.println("Invalid input: " + e.getMessage());
+        } catch (NoSuchElementException | IllegalStateException e) {
+            System.out.println(e);
+        }
+        finally {
+            scanner.close();
+        }
+
+    }
 
 }
